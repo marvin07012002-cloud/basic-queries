baaea261d9de18d5a4618e0633e09541ebee103b
marbin701 <marvin07012002@gmail.com>
2026-06-02 12:58:24 -0400

    16 done

---
+++ 16_Products_table.sql
@@ -0,0 +1,14 @@
+/*supplies each
+product*/
+select SupplierID, ProductName
+from Products;
+/*Tokyo Traders" supplies to
+Northwind*/
+select SupplierID, ProductName
+from Products
+where SupplierID = 4;
+
+
+
+
+
